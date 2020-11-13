89994de016115198f95f09c5dfe10c17b3cd3d1b
KendaCodes <K.Lamandier@gmail.com>
2020-11-12 19:15:39 -0500

    Pushing the codewars for week one

---
+++ codewarday1.js
@@ -0,0 +1,13 @@
+// CONVERT NUMBER TO STRING
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// 4 = '4'
+// o(1) time complexity
+function numberToString(num) {
+    return num + '';
+  }
+
+  numberToString();
